573be553c6eee15d583e940d5519bd5d5b04a71f
Ethan Zhu <ethanyichenzhu@gmail.com>
2026-03-19 11:57:16 -0400

    done

--- CCC '10 S2 - Huffman Encoding.py
+++ CCC '10 S2 - Huffman Encoding.py
@@ -1,3 +1,18 @@
 rounds = int(input())
+dic = {}
+queue = ''
+answer = ''
 for i in range(rounds):
     text = input().split()
+    dic[text[1]] = text[0]
+
+code = input()
+for u in range(len(code)):
+    queue+= code[u]
+    
+    if queue in dic:
+        answer = answer + dic[queue]
+        queue = ''
+        
+print(answer)
+
